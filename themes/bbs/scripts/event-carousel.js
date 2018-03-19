1d8c9ffa868c029da4437b0ed1d169822b56e85b
Gauti Nils <gauti@reon.is>
2018-03-19 14:22:23 +0000

    BBS-Carousel

---
+++ themes/bbs/scripts/event-carousel.js
@@ -0,0 +1,18 @@
+(function (scope, $) {
+    'use strict';
+    Drupal.behaviors.misc = {
+        attach: function () {
+            var quicktabs_lists = $('#quicktabs-events .item-list ul'),
+                tabs = $('#quicktabs-events .quicktabs-tab')
+
+            quicktabs_lists.each(function () {
+                $(this).not('.slick-initialized').slick({'slidesToShow': 3, 'slidesToScroll': 1, 'infinite': false});
+            });
+            tabs.on('click', function() {
+                quicktabs_lists.each(function () {
+                    $(this).get(0).slick.setPosition();
+                });
+            });
+        }
+    };
+})(this, jQuery);
