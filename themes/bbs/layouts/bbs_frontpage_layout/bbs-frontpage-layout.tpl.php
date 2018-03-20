392e2dcc66fadcef93342d03892caade1ee5fd75
Gauti Nils <gauti@reon.is>
2018-03-20 17:08:16 +0000

    Frontpage border + Flex

--- themes/bbs/layouts/bbs_frontpage_layout/bbs-frontpage-layout.tpl.php
+++ themes/bbs/layouts/bbs_frontpage_layout/bbs-frontpage-layout.tpl.php
@@ -10,43 +10,47 @@
  */
 ?>
 <div class="frontpage-content">
-    <div class="main-content">
-        <div class="row">
-            <div class="left-side">
-                <?php if (!empty($content['menu'])): ?>
-                    <div class="menu-wrapper">
-                        <?php print $content['menu']; ?>
+    <div class="border-wrapper">
+        <div class="border-element">
+            <div class="main-content">
+                <div class="row">
+                    <div class="left-side">
+                        <?php if (!empty($content['menu'])): ?>
+                            <div class="menu-wrapper">
+                                <?php print $content['menu']; ?>
+                            </div>
+                        <?php endif ?>
                     </div>
-                <?php endif ?>
-            </div>
-            <div class="right-side">
-                <?php if (!empty($content['main-info'])): ?>
-                    <div class="main-info">
-                        <?php print $content['main-info']; ?>
+                    <div class="right-side">
+                        <?php if (!empty($content['main-info'])): ?>
+                            <div class="main-info">
+                                <?php print $content['main-info']; ?>
+                            </div>
+                        <?php endif ?>
                     </div>
-                <?php endif ?>
-            </div>
-        </div>
-        <div class="row">
-            <div class="left-side">
-                <?php if (!empty($content['new-user'])): ?>
-                    <div class="new-user">
-                        <?php print $content['new-user']; ?>
+                </div>
+                <div class="row">
+                    <div class="left-side">
+                        <?php if (!empty($content['new-user'])): ?>
+                            <div class="new-user">
+                                <?php print $content['new-user']; ?>
+                            </div>
+                        <?php endif ?>
                     </div>
-                <?php endif ?>
-            </div>
-            <div class="right-side">
-                <div class="secondary-info">
-                    <?php if (!empty($content['sec-info'])): ?>
-                        <div class="secondary-info-item">
-                            <?php print $content['sec-info']; ?>
+                    <div class="right-side">
+                        <div class="secondary-info">
+                            <?php if (!empty($content['sec-info'])): ?>
+                                <div class="secondary-info-item">
+                                    <?php print $content['sec-info']; ?>
+                                </div>
+                            <?php endif ?>
                         </div>
-                    <?php endif ?>
+                    </div>
                 </div>
             </div>
+            <img class="border-image" src="profiles/ding2/themes/bbs/images/icons/logo-black-grey@2x.png"/>
         </div>
     </div>
-
     <div class="secondary-content">
         <?php if (!empty($content['secondary-content'])): ?>
             <?php print $content['secondary-content']; ?>
--- themes/bbs/scripts/menu.js
+++ themes/bbs/scripts/menu.js
@@ -10,9 +10,10 @@
                 close_user_login = $('.close-user-login', context),
                 body = $('body'),
                 topbar_menu = $('.topbar-link-menu-inner'),
+                topbar = $('.topbar'),
                 main_menu_button = $('a.menu-button'),
                 sub_menu = $('.sub-menu'),
-                ddbasic;
+                top = 80;
 
             topbar_menu_btn.on('click', function (evt) {
                 evt.preventDefault();
@@ -22,6 +23,8 @@
 
             topbar_link_user.on('click', function (evt) {
                 evt.preventDefault();
+                body.removeClass('menu-is-open');
+                topbar_menu.removeClass('active');
                 ddbasic.openLogin();
             });
 
@@ -31,7 +34,7 @@
                 body.removeClass('overlay-is-active');
             });
 
-            main_menu_button.on('click', function(evt) {
+            main_menu_button.on('click', function (evt) {
                 evt.preventDefault();
 
                 // remove previously expanded
@@ -42,6 +45,26 @@
                 $(this).parent().find('.sub-menu').removeClass('hidden');
 
             });
+
+            if (body.hasClass('front')) {
+                if ($(window).scrollTop() >= top) {
+                    topbar.css('top', 0);
+                }
+
+                window.onscroll = function () {
+                    var yOffset = $(window).scrollTop();
+                    if (yOffset >= top) {
+                        topbar.css('top', 0);
+                    }
+                    else {
+                        topbar.css('top', top - yOffset);
+                    }
+                }
+            }
+            else {
+                topbar.css('top', 0);
+            }
+
         }
     };
 })(this, jQuery);
