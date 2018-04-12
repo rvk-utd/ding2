e913b2d457000b60611dfc395558217c4974b607
agust <agust@reoncloud.com>
2018-04-12 17:34:37 +0000

    bbs-pages style and literature

--- themes/bbs/templates/node/node--ding-page.tpl.php
+++ themes/bbs/templates/node/node--ding-page.tpl.php
@@ -85,19 +85,14 @@
  */
 ?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-    <?php if(!empty($node->field_ding_page_title_image)): ?>
 
-    <img class="title-image" src="<?php print file_create_url($node->field_ding_page_title_image['und'][0]['uri']); ?>"/>
+    <div class="bbs-top-page-content">
+      <div class="bbs-page-banner-image" style="background-image: url(<?php print file_create_url($node->field_ding_page_title_image['und'][0]['uri']); ?>);"/></div>
 
-    <?php endif; ?>
-
-    <h2 class="page-title"><?php print $title; ?></h2>
-
-    <div class="node-lead">
-        <?php print render($content['field_ding_page_lead']) ?>
-    </div>
-    <div class="node-body">
+      <div class="bbs-page-text-wrapper">
+        <h2 class="page-title"><?php print $title; ?></h2>
         <?php print render($content['field_ding_page_body']) ?>
+      </div>
     </div>
 
 </div>
