285e1d7cb3503762b6e215e6503b1fdb490b3b20
Gauti Nils Bernhardsson <gauti@reontech.com>
2018-04-27 14:40:37 +0000

    Add classes to ting object

--- themes/bbs/templates/ting_object/ting-object--view-mode--teaser.tpl.php
+++ themes/bbs/templates/ting_object/ting-object--view-mode--teaser.tpl.php
@@ -10,7 +10,7 @@
  */
  hide($content['ding_serendipity_info']);
 ?>
-<div class="book">
+<div class="<?php print $classes; ?> book">
   <div class="inner">
     <a href="<?php print $ting_object_url_object; ?>">
       <?php print render($content['ting_cover']); ?>
