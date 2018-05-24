a70acc594e6892d50b2eed90e1160d912dd3c86c
agust <agust@reoncloud.com>
2018-05-24 15:32:01 +0000

    more styling for search, contact, ting/object, nyheder and bbs-layout components

--- themes/bbs/layouts/bbs_frontpage_layout/bbs-frontpage-layout.tpl.php
+++ themes/bbs/layouts/bbs_frontpage_layout/bbs-frontpage-layout.tpl.php
@@ -10,70 +10,75 @@
  */
 ?>
 <div class="bbs-layout-content">
-    <div class="border-wrapper">
-        <div class="border-element">
-            <div class="main-content content-block">
-                <div class="content-wrapper">
-                    <div class="row">
-                        <div class="left-side">
-                            <?php if (!empty($content['menu'])): ?>
-                                <div class="menu-wrapper">
-                                    <?php print $content['menu']; ?>
-                                </div>
-                            <?php endif ?>
-                        </div>
-                        <div class="right-side">
-                            <?php if (!empty($content['main-info'])): ?>
-                                <div class="main-info">
-                                    <?php print $content['main-info']; ?>
-                                </div>
-                            <?php endif ?>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="left-side">
-                            <?php if (!empty($content['new-user'])): ?>
-                                <div class="new-user">
-                                    <?php print $content['new-user']; ?>
-                                </div>
-                            <?php endif ?>
-                        </div>
-                        <div class="right-side">
-                            <div class="secondary-info">
-                                <?php if (!empty($content['sec-info'])): ?>
-                                    <div class="secondary-info-item">
-                                        <?php print $content['sec-info']; ?>
-                                    </div>
-                                <?php endif ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <img class="border-image" src="profiles/ding2/themes/bbs/images/icons/logo-black-grey@2x.png"/>
-        </div>
-    </div>
-    <div class="content-block secondary-content">
+  <div class="border-wrapper">
+    <div class="border-element">
+      <div class="main-content content-block">
+        <?php if (!empty($content['title'])): ?>
+          <h1>
+            <?php print $content['title']; ?>
+          </h1>
+        <?php endif ?>
         <div class="content-wrapper">
-            <?php if (!empty($content['secondary-content'])): ?>
-                <?php print $content['secondary-content']; ?>
-            <?php endif ?>
+          <div class="row">
+            <div class="left-side">
+              <?php if (!empty($content['menu'])): ?>
+                <div class="menu-wrapper">
+                  <?php print $content['menu']; ?>
+                </div>
+              <?php endif ?>
+            </div>
+            <div class="right-side">
+              <?php if (!empty($content['main-info'])): ?>
+                <div class="main-info">
+                  <?php print $content['main-info']; ?>
+                </div>
+              <?php endif ?>
+            </div>
+          </div>
+          <div class="row">
+            <div class="left-side">
+              <?php if (!empty($content['new-user'])): ?>
+                <div class="new-user">
+                  <?php print $content['new-user']; ?>
+                </div>
+              <?php endif ?>
+            </div>
+            <div class="right-side">
+              <div class="secondary-info">
+                <?php if (!empty($content['sec-info'])): ?>
+                  <div class="secondary-info-item">
+                    <?php print $content['sec-info']; ?>
+                  </div>
+                <?php endif ?>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+      <img class="border-image" src="profiles/ding2/themes/bbs/images/icons/logo-black-grey@2x.png"/>
     </div>
-    <div class="logo-wrapper red-background">
+  </div>
+  <div class="content-block secondary-content">
+    <div class="content-wrapper">
+      <?php if (!empty($content['secondary-content'])): ?>
+        <?php print $content['secondary-content']; ?>
+      <?php endif ?>
+    </div>
+  </div>
+  <div class="logo-wrapper red-background">
     <div class="content-block tertiary-content ">
-        <div class="content-wrapper ">
-            <?php if (!empty($content['tertiary-content'])): ?>
-                <?php print $content['tertiary-content']; ?>
-            <?php endif ?>
-        </div>
+      <div class="content-wrapper ">
+        <?php if (!empty($content['tertiary-content'])): ?>
+          <?php print $content['tertiary-content']; ?>
+        <?php endif ?>
+      </div>
     </div>
     <?php if (!empty($content['quaternary-content'])): ?>
-        <div class="content-block quaternary-content">
-            <div class="content-wrapper ">
-                <?php print $content['quaternary-content']; ?>
-            </div>
+      <div class="content-block quaternary-content">
+        <div class="content-wrapper ">
+          <?php print $content['quaternary-content']; ?>
         </div>
+      </div>
     <?php endif ?>
-    </div>
+  </div>
 </div>
