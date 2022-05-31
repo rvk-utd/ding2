cb42e633cca7f1cee5f32a121ba45ef7b059e147
Jakob Larsen <jakob@revealit.dk>
2022-05-31 21:53:21 +0200

    BBS-562 Fix issue with ding handling of availability

--- modules/ding_availability/js/ding_availability_labels.js
+++ modules/ding_availability/js/ding_availability_labels.js
@@ -57,7 +57,12 @@
         // Apply already fetched availability, if any.
         if (settings.hasOwnProperty('ding_availability')) {
           $.each(settings.ding_availability, function(id, entity_ids) {
-            update_availability(id, entity_ids);
+            // This can be triggered eventhough availability is not yet
+            // fetched, which means it will show up as unavailable. So skip
+            // items where we don't know if available or unavailable.
+            if (Drupal.DADB[entity_id] !== null) {
+              update_availability(id, entity_ids);
+            }
           });
           update_availability_remove_pending();
         }
